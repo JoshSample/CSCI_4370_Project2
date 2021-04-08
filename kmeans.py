d72fab84c17846744c0f01261886b6335319efcb
JoshSample <joshsample98@gmail.com>
2021-04-08 00:47:19 -0500

    Added methods and made small progress on kmeans

--- kmeans.py
+++ kmeans.py
@@ -1,20 +1,40 @@
 # Data Mining Project 2
-# # By Makenzie Spurling, Josh Sample, and Kathryn Villarreal
+# By Makenzie Spurling, Josh Sample, and Kathryn Villarreal
 import numpy as np
+import pandas as pd
 
+# find distance between points
 def euclidean_distance(point1, point2):
     return np.sqrt(np.sum(np.square(point1-point2)))
 
-def init_centroids():
+# select random centroids based on number of clusters
+def init_centroids(k, df):
+    return df.sample(n = k)
+
+# method for seeing if there was improvement
+def sum_of_squared_errors():
     pass
+
+# clustering algorithm
+def kmeans(k, n, iterations, df):
+    centroids = init_centroids(k, df)
+    
+    # iterate until no more improvement
+    for i in range(iterations):
+        # assign points to clusters
+        for j in range(n):
 
 
 
 
 if __name__ == "__main__":
+    # create dataframe from dataset
+    df = pd.read_excel("output.xls")
     # number of clusters
     k = 3
     # number of objects
-    n = 0 #obtained from file
+    n = len(df.index)
     # number of iterations 
     i = 100
+    # pass data to algorithm
+    kmeans(k, n, i, df)
