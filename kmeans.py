47772f807d9b5f557990f26ae743eaf5e8993940
JoshSample <joshsample98@gmail.com>
2021-04-08 19:59:25 -0500

    Need better data structure for clusterdPoints

--- kmeans.py
+++ kmeans.py
@@ -3,26 +3,48 @@
 import numpy as np
 import pandas as pd
 
+sse = 0
+
 # find distance between points
 def euclidean_distance(point1, point2):
-    return np.sqrt(np.sum(np.square(point1-point2)))
+    total = 0
+    for i in range(3):
+        total += np.sqrt(np.sum(np.square(point1[i+4]-point2[i+4])))
+    return total
 
 # select random centroids based on number of clusters
 def init_centroids(k, df):
     return df.sample(n = k)
 
 # method for seeing if there was improvement
-def sum_of_squared_errors():
-    pass
+def sum_of_squared_errors(k, n, clusteredPoints, centroids):
+    sse = 0
+    for i in range(k):
+        for j in range(n):
+            if clusteredPoints[i][j] != 0:
+                sse += euclidean_distance(clusteredPoints[i][j], centroids.iloc[i])
+    return sse
 
 # clustering algorithm
 def kmeans(k, n, iterations, df):
     centroids = init_centroids(k, df)
-    
     # iterate until no more improvement
     for i in range(iterations):
+        clusteredPoints = [[0 for x in range(k)] for y in range(n)]
         # assign points to clusters
-        for j in range(n):
+        for index, row in df.iterrows():
+            min = 2
+            for j in range(k):
+                distance = euclidean_distance(row, centroids.iloc[j])
+                if distance < min:
+                    min = distance
+                    closest = j
+            clusteredPoints[closest][index] = row
+        sse1 = sse
+        sse2 = sum_of_squared_errors(k, n, clusteredPoints, centroids)
+        if (sse1 - sse2) / sse1 < 0.0001:
+            break
+    pass
 
 
 
