19987f4cdc7b2935622cf360f65149836c718f50
JoshSample <joshsample98@gmail.com>
2021-04-22 22:58:41 -0500

    Added more data to output file

--- kmeans.py
+++ kmeans.py
@@ -29,9 +29,9 @@ def calc_centroids(clusters, X):
     return new_centroids
 
 # output clustered data to Excel
-def output_data(clusters, X, k):
+def create_df(clusters, X):
     new_df = pd.concat([pd.DataFrame(X), pd.DataFrame(clusters, columns=['cluster'])], axis=1)
-    new_df.to_excel("kmeans with " + str(k) + " clusters.xls")
+    return new_df
 
 # clustering algorithm
 def kmeans(k, n, iterations, X):
@@ -49,14 +49,18 @@ def kmeans(k, n, iterations, X):
         centroids = calc_centroids(get_centroids, X)
         # when the centroids don't change, the algorithm has converged
         if np.array_equal(centroids, old_centroids):
-            output_data(get_centroids, X, k)
-            break
+            new_df = create_df(get_centroids, X)
+            return new_df
 
     
 # driver: need dataset, k, and i
 if __name__ == "__main__":
     # create dataframe from dataset
-    df = pd.read_excel("output.xls")
+    df = pd.read_excel("filtered_output.xls")
+    # get list of candidate genese
+    candidate_genes = df.iloc[[0, 86]]
+    # get list of longevity genes
+    longevity_genes = df.iloc[[87, 159]]
     # create array from data
     X = df[["sch9/wt", "ras2/wt", "tor1/wt"]].to_numpy()
     # number of clusters
@@ -66,4 +70,9 @@ if __name__ == "__main__":
     # number of iterations 
     i = 100
     # pass data to algorithm
-    kmeans(k, n, i, X)
+    new_df = kmeans(k, n, i, X)
+    # add cluster data to dataframe
+    cluster = new_df[["cluster"]]
+    df['cluster'] = cluster
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    df.to_excel("kmeans with " + str(k) + " clusters.xls")
