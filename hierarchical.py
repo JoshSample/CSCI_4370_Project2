540afb1476308d006fe4fc37e88a3f1b93b22f3f
Kathryn Villarreal <katnvillarreal@gmail.com>
2021-04-29 12:34:28 -0500

    Updated the hierahcical code and got my clusters

--- hierarchical.py
+++ hierarchical.py
@@ -2,80 +2,86 @@
 # By Makenzie Spurling, Josh Sample, and Kathryn Villarreal
 
 import pandas as pd
-import collections
-
-def get_distance(point1, point2):
-    # Given two points get the distance between them
-    d = []
-    for i in range(len(point1)):
-        d.append(abs(point1[i] - point2[i]))
-    return d
-
-def get_new_center(point1, point2):
-    c = []
-    for i in range(len(point1)):
-        c.append((point1[i] + point2[i])/2)
-    return c
-
-def heir(df, num_clusters):
-    s = df["sch9/wt"].tolist()
-    r = df["ras2/wt"].tolist()
-    t = df["tor1/wt"].tolist()
-    points = dict()
-    # Get all vectors from the three columns
-        # Add each vector to a dictionary
-    for i in range(len(s)):
-        points[i] = [s[i],r[i],t[i]]
-    while True:
-        # Calculate the distance vector between each vector
-        distances = dict()
-        for i in range(len(points)):
-            for j in range(i+1, len(points)):
-                k1 = list(points.keys())[i]
-                k2 = list(points.keys())[j]
-                tup = (k1, k2)
-                distances[tup] = get_distance(points[k1],points[k2])
-        # Find the smallest distance vector
-        sort = {k: v for k, v in sorted(distances.items(), key=lambda item: item[1])}
-        # Find the center between those two points
-        first_k = list(sort.keys())[0]
-        p1 = points[first_k[0]]
-        p2 = points[first_k[1]]
-        new_point = get_new_center(p1,p2)
-        # Add that new center point to the dictionary
-        points[first_k] = new_point
-        # Remove the two points from before 
-        del points[first_k[0]]
-        del points[first_k[1]]
-        for d in list(distances.keys()):
-            if first_k[0] in d:
-                del distances[d]
-            elif first_k[1] in d:
-                del distances[d]
-
-        if len(points) <= int(num_clusters):
-            return points
+import numpy as np
                 
-def write_to_excel(num_clusters, clusters, df, file_name):
-    keys = list(clusters.keys())
-    output = df[["sch9/wt", "ras2/wt", "tor1/wt"]]
-    l = len(df.index)
-    c = [None] * l
-    for i in range(int(num_clusters)):
-        k = keys[i]
-        p = str(k)
-        p = p.replace('(','')
-        p = p.replace(')','')
-        p = p.replace(' ', '')
-        index = p.split(',')
-        for v in index:
-            c[int(v)] = i
-
-    output["Cluster"] = c
+def write_to_excel(clusters, df, file_name):
+    output = df[["Public ID", "Gene", "Gene description", "sch9/wt", "ras2/wt", "tor1/wt"]]
+    cluster_keys = clusters.keys()
+    i = 0
+    l = [None] * len(output.index)
+    for c in cluster_keys:
+        for index in c:
+            l[int(index)] = i
+        i = i + 1
+    output["Clusters"] = l
+    #output = output.sort_values(by=['Clusters'])
+    #print(output)
+    output = output.rename_axis('MyIdx').sort_values(by = ['Clusters', 'MyIdx'])
     output.to_excel(file_name)
+
+def heir(points, num_clusters):
+    clusters = dict()
+    for i in range(len(points)):
+        clusters[i] = points[i]
+    while True:
+        # Get a list of clusters keys
+        k = list(clusters.keys())
+        # if length of clusters = num_clusters
+        if len(k) == num_clusters:
+            # return dict of clusters and new centroids
+            # {
+            #   [index, index, index, ....]: [x,y,z],
+            #   [index, index, index, ....]: [x,y,z],
+            #   [index, index, index, ....]: [x,y,z] 
+            # }
+            final = dict()
+            for keys in k:
+                p = str(keys)
+                p = p.replace('(','')
+                p = p.replace(')','')
+                p = p.replace(' ', '')
+                index = tuple(p.split(','))
+                final[index] = clusters[keys]
+            return final
+        # else
+        else: 
+            # Get the distance between each point
+            dist = dict()
+            for i in range(len(k)):
+                for j in range(i+1, len(k)):
+                    tup = (k[i],k[j])
+                    dist[tup] = euclidean_distance(clusters[k[i]], clusters[k[j]])
+            # Get the smallest distance
+            smallest = min(dist, key=dist.get)
+            # Get the new center between the two closest points
+            #new_center = find_center(clusters[smallest[0]],clusters[smallest[1]])
+            array = np.append([clusters[smallest[0]]], [clusters[smallest[1]]], axis=0)
+            new_center = array.mean(axis=0)
+            # Add that to the clusters dictionary
+            clusters[smallest] = new_center
+            # Remove the old indexes from clusters
+            del clusters[smallest[0]]
+            del clusters[smallest[1]]
+
+# find new center
+def find_center(p1,p2):
+    l = list()
+    for i in range(len(p1)):
+        p = (p1[i]+p2[i])/2
+        l.append(p)
+    a = np.array(l)
+    return a
+
+# find distance between points
+def euclidean_distance(point1, point2):
+    return(sum((point1 - point2) ** 2)) ** 0.5
 
 if __name__ == "__main__":
     df = pd.read_excel("filtered_output.xls")
-    num_clusters = 3
-    clusters = heir(df, num_clusters)
-    write_to_excel(num_clusters, clusters, df, "filtered_h_3_clusters.xlsx")
+    # create array from data
+    points = df[["sch9/wt", "ras2/wt", "tor1/wt"]].to_numpy()
+    # number of desired clusters
+    num_clusters = 7
+
+    clusters= heir(points, num_clusters)
+    write_to_excel(clusters, df, "filtered_h_7_clusters.xlsx")
